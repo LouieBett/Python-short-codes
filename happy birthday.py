2e751ccb8e4cb4a88821fd03b72e58caf4dcc358
LouieBett <TRA20BettL@threerivers.thpt.org.uk>
2024-10-01 11:46:17 +0100

    happy birthday

--- happy birthday.py
+++ happy birthday.py
@@ -7,25 +7,35 @@ input()
 
 print("Enter needed information below")
 
-def day_back():
-    try:
-        day = int(input("What day were you born (Number): "))
-    except ValueError:
-        print("Invalid input. Please enter a NUMBER.")
-        balls()
-def month_back():
-    try:
-        month = int(input("What month were you born in?(Number) "))
-    except ValueError:
-        print("Invalid input. Please enter a NUMBER.")
-def year_back():
-    try:
-        year = int(input("What year were you born in? "))
+def get_day():
+    while True:
+        try:
+            day = int(input("What day were you born (Number): "))
+            return day
+        except ValueError:
+            print("Invalid input. Please enter a NUMBER.")
 
-    except ValueError:
-        print("Invalid input. Please enter a NUMBER.")
+def get_month():
+    while True:
+        try:
+            month = int(input("What month were you born in? (Number) "))
+            return month
+        except ValueError:
+            print("Invalid input. Please enter a NUMBER.")
+
+def get_year():
+    while True:
+        try:
+            year = int(input("What year were you born in? "))
+            return year
+        except ValueError:
+            print("Invalid input. Please enter a NUMBER.")
+
+day = get_day()
+month = get_month()
+year = get_year()
 
 print("Loading...")
 time.sleep(3)
 
-print("You were born on: ",day,"/",month,"/",year,"Your age is: ",2024-year)
+print("You were born on: ", day, "/", month, "/", year, "Your age is: ", 2024 - year,)
