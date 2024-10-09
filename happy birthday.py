0f5320ad7966cbbb7aecd0981f89420ba7ff24f4
LouieBett <TRA20BettL@threerivers.thpt.org.uk>
2024-10-09 12:33:16 +0100

    happy birthday update 2.1

--- happy birthday.py
+++ happy birthday.py
@@ -7,28 +7,31 @@ input()
 
 print("Enter needed information below")
 
+#This asks for a user to enter the day they were born on
 def get_day():
     while True:
         try:
             day = int(input("What day were you born (Number): "))
-            return day
-        except ValueError:
+            return day #This makes it go back to the start of the def
+        except ValueError: #This ignores the error
             print("Invalid input. Please enter a NUMBER.")
+#This asks for a user to enter the month they were born in
 
 def get_month():
     while True:
         try:
             month = int(input("What month were you born in? (Number) "))
-            return month
-        except ValueError:
+            return month #This makes it go back to the start of the def
+        except ValueError: #This ignores the error
             print("Invalid input. Please enter a NUMBER.")
 
+#This asks for a user to enter the year they were born in
 def get_year():
     while True:
         try:
             year = int(input("What year were you born in? "))
-            return year
-        except ValueError:
+            return year #This makes it go back to the start of the def
+        except ValueError: #This ignores the error
             print("Invalid input. Please enter a NUMBER.")
 
 day = get_day()
